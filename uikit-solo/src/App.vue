505c286234e8420c4f13ac01381b2230f278e4dd
Pesottsky <pesottsky@gmail.com>
2023-09-02 15:08:32 +0300

    test2
    test2

---
+++ uikit-solo/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import ButtonUI from './components/ButtonUI.vue'
+import IconBase from './components/IconBase.vue'
+import IconWrite from './components/icons/IconWrite.vue'
+</script>
+
+<template>
+  <h1 class="heading-1">Кнопки и цвета</h1>
+  
+  <div class="line"> 
+    <ButtonUI label="Primary" color="primary"/>
+    <ButtonUI label="Secondary" color="secondary"/>
+    <ButtonUI label="Tetrary" color="tetrary" />
+    <icon-base icon-name="write"><icon-write /></icon-base>
+  </div>
+  
+</template>
+
+<style scoped>
+.line{
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+  margin: 20px 0px;
+}
+</style>
---
+++ uikit-solo/src/components/ButtonUI.vue
@@ -0,0 +1,50 @@
+<template>
+    <button :class="['ButtonUI', `ButtonUI_${color}`]"
+    >{{ label }} 
+   
+    </button>
+</template>
+
+<script setup>
+
+    const props = defineProps ({
+        label: {
+            type: String,
+            default: 'Press me'
+        },
+        color:{
+            type: String,
+            default: 'primary'
+        },
+        
+    })
+
+</script>
+
+<style>
+.ButtonUI{
+    display: inline-flex;
+    padding: 8px 12px;
+    justify-content: center;
+    align-items: center;
+    gap: 4px;
+
+    border-radius: 8px;
+    border: 1px solid var(--black);
+    background: var(--black);
+    color: var(--white);
+
+    &:hover{
+        background: var(--white);
+    }
+   
+}
+.ButtonUI_secondary {
+        background: var(--white);
+        border: 1px solid #000;
+    }
+.ButtonUI_tetrary {
+        border: 1px solid #000;
+        background: #f50202;
+    }
+</style>
