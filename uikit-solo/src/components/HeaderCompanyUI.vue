71ac1ab966d91413592d1748e9d26b84bd2ea365
Pesottsky <pesottsky@gmail.com>
2023-09-10 23:47:34 +0300

    Headers components

---
+++ uikit-solo/src/components/HeaderCompanyUI.vue
@@ -0,0 +1,36 @@
+<script setup>
+import ButtonUI from './ButtonUI.vue'
+
+</script>
+
+<template>
+    
+   <header>
+    <div class="header-Company-UI">
+        <h2>Фрилансеры</h2>
+        <div class="header-buttons">
+            <ButtonUI label="Добавить" color="tetrary" icon="url(../src/assets/icons/add-person.svg) no-repeat"/>
+            <ButtonUI label="Пригласить" color="tetrary" icon="url(../src/assets/icons/import-black.svg) no-repeat" />
+            <ButtonUI label="" color="tetrary" icon="url(../src/assets/icons/big-three-dots-black.svg) no-repeat" />
+        </div>
+    </div>
+   </header>
+</template>
+
+<style scoped>
+.header-Company-UI {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    width: 1023px;
+    height: 40px;
+    padding: 0px 0px 0px 48px;
+    gap: 8px;
+}
+.header-buttons {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+}
+</style>
